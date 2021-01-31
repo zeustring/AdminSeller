3d836ad95687ee01260720d22e32aa01ead2afc5
Cristian Joel Hernández Hernández <zeustring@gmail.com>
2021-01-31 00:30:47 -0600

    Update index.blade.php

--- resources/views/Empleados/index.blade.php
+++ resources/views/Empleados/index.blade.php
@@ -2,7 +2,7 @@
 @section('contend')
     <div class="card">
               <div class="card-header">
-                <h3 class="card-title">Listado de Empleados</h3>
+                <h3 class="card-title">Listado de Empleadosss</h3>
               </div>
               <!-- /.card-header -->
               <div class="card-body">
