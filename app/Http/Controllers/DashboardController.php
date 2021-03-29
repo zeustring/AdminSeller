83bba83be52ec46ba5d33b4df34d22ff6049ea53
Cristian Joel Hernández Hernández <zeustring@gmail.com>
2021-03-28 22:52:04 -0600

    se genera dashboard
    se implementa nuevo dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,12 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Auth;
 use App\Membresia;
+use App\Carta;
+use App\Cliente;
 class DashboardController extends Controller
 {
     public function index()
     {  
-        $membresia     =     Membresia::where('id_empleado','=',Auth::user()->id)->first();
-    	return view('dashboard.index',['membresia' => $membresia]);
+        $cartasCount            =        Carta::where('id_empleado','=',Auth::user()->id)->count();
+        $clientesCount          =        Cliente::where('id_empleado','=',Auth::user()->id)->count();
+    	return view('dashboard.index',['cartasCount'   => $cartasCount,
+    								   'clientesCount' => $clientesCount]);
     }
 
 
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,7 +1,108 @@
 @extends('layouts.app-menu')
 @section('contend')
-
+  <div class="alert alert-info alert-dismissible">
+                 
+                  <h5><i class="icon fas fa-tachometer-alt"></i> Mi Dashboard</h5>
+                 
+  </div>
+<div class="row">
+<style type="text/css">
+  @media (max-width: 650px) {
+  .inner h3{
+   position: relative;
+   text-align: left;
+   margin-left: 0px;
+   margin-top: 5px;
+   font-size: 25px;
+  }
+  .fass {
+    display: block;
+    color: #FFF;
+    font-size: 50px;
+    float: right;
+    position: relative;
+    margin-top: 10px;
+    margin-right: 5px;
     
+  }
+ .inner p{
+  position: relative;
+  font-size: 13px;
+  text-align: left;
+ }
+}
+</style>
+
+<div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+                 <div class="icono">
+                <i class="fass fas fa-envelope"></i>
+              </div>
+                <h3>{{$cartasCount}}</h3>
+
+                <p>Mis Cartas</p>
+              </div>
+              
+              
+            </div>
+</div>
+<div class="col-lg-3 col-6">             
+           
+             <div class="small-box bg-warning">
+              <div class="inner">
+                <div class="icono">
+                <i class="fass fas fa-users"></i>
+              </div>
+                <h3>{{$clientesCount}}</h3>
+
+                <p>Mis Clientes</p>
+              </div>
+              
+             
+            </div>
+</div>
+</div>
+  <div class="alert bg-maroon alert-dismissible">
+                 
+                  <h5><i class="icon fas fa-tachometer-alt"></i>Dashboard de Sucursal</h5>
+                 
+  </div>
+<div class="row">
+<div class="col-lg-3 col-6">             
+           
+             <div class="small-box bg-navy">
+              <div class="inner">
+              <div class="icono">
+                <i class="fass fas fa-envelope"></i>
+              </div>
+                <h3>{{$clientesCount}}</h3>
+                <i></i>
+                <p>Cartas de Sucursal</p>
+              </div>
+
+             
+             
+            </div>
+</div>
+<div class="col-lg-3 col-6">             
+           
+             <div class="small-box bg-orange">
+              <div class="inner">
+                <div class="icono">
+                <i class="fass fas fa-users"></i>
+              </div>
+                <h3>{{$clientesCount}}</h3>
+
+                <p>Clientes Sucursal</p>
+              </div>
+              
+             
+            </div>
+</div>
+</div>
+
       
 
 @endsection
