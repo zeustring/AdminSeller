5248b5b6225dce85f6bbe89e5989f366201eef8f
Cristian Joel Hernández Hernández <zeustring@gmail.com>
2021-07-26 23:16:14 -0500

    se lanza nueva version de admin seller
    nueva version de admniseller 2.1

--- resources/views/layouts/app-menu.blade.php
+++ resources/views/layouts/app-menu.blade.php
@@ -312,7 +312,7 @@
 
   <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-      <b>Version</b> 1.1
+      <b>Version</b> 2.1
     </div>
     <strong>Copyright &copy; 2021 AdminSeller </strong> powered by zeustring
   </footer>
